2ed0ea632cc6311aa2e0cc82e67e7edcb8c626a0
Krist Ponpairin <krist7599555@gmail.com>
2022-03-13 23:02:58 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/**
+ * https://www.onlinegdb.com/edit/HYTv2BeqJ
+ * https://codepen.io/krist7599555/pen/OJzJBML
+ * https://github.com/krist7599555/regularpolygon-stacker
+ */
 #include <iostream>
 #include <string>
 #include <utility>
@@ -11,15 +16,9 @@ public:
   double x;
   double y;
   Point(double _x, double _y): x(_x), y(_y) {}
-  friend Point operator + (const Point& lhs, const Point& rhs) {
-    return Point(lhs.x + rhs.x, lhs.y + rhs.y);
-  }
-  friend Point operator * (const double& scala, const Point& point) {
-    return Point(point.x * scala, point.y * scala);
-  }
-  friend Point operator * (const Point& point, const double& scala) {
-    return Point(point.x * scala, point.y * scala);
-  }
+  friend Point operator + (const Point& lhs, const Point& rhs) { return Point(lhs.x + rhs.x, lhs.y + rhs.y); }
+  friend Point operator * (const double& scala, const Point& point) { return Point(point.x * scala, point.y * scala); }
+  friend Point operator * (const Point& point, const double& scala) { return Point(point.x * scala, point.y * scala); }
   static Point from_angle(double radius) {
     return Point(cos(radius), sin(radius));
   }
@@ -30,47 +29,41 @@ public:
 
 template<typename T>
 std::ostream& operator << (std::ostream& o, const std::vector<T>& vec) {
-    o << "[";
-    for (int i = 0; i < vec.size(); ++i) {
-      o << vec[i];
-      if (i < vec.size() - 1) {
-        o << ", ";
-      }
+  o << "[";
+  for (int i = 0; i < vec.size(); ++i) {
+    o << vec[i];
+    if (i < vec.size() - 1) {
+      o << ", ";
     }
-    return o << "]";
   }
+  return o << "]";
+}
 
 class RegularPolygon {
 public:
   int n; // n >= 3, flat base
   double side_length; // 2 * circum_radius * sin(pi/n)
-  double circum_radius; // 1/2 * side_length * csc(pi/n)
   Point center;
   
   RegularPolygon(int _n, int _side_length):
     n(_n),
     side_length(_side_length),
-    circum_radius(0.5 * side_length * (1.0 / sin(M_PI / n))),
     center(0, 0) {}
   /**
    *   /\      __
    * /   \   /   \
    * \__/    \__/
    */
-  double in_radius() const {
-    return circum_radius * cos(M_PI / n);
-  }
-  double exterior_angle() const {
-    return 2 * M_PI / n;
-  }
-  double interior_angle() const {
-    return (n - 2) * M_PI / n;
-  }
+  double circum_radius() const { return 0.5 * side_length * (1.0 / sin(M_PI / n)); }
+  double in_radius() const { return circum_radius() * cos(M_PI / n); }
+  double exterior_angle() const { return 2 * M_PI / n; }
+  double interior_angle() const { return (n - 2) * M_PI / n; }
+  
   std::vector<Point> to_points() const {
     std::vector<Point> res;
     double starter_radius = (M_PI * 1.5) - (M_PI / n);
     for (int i = 0; i < n; ++i) {
-      res.push_back(center + Point::from_angle(2.0 * M_PI / n * i + starter_radius) * circum_radius);
+      res.push_back(center + Point::from_angle(2.0 * M_PI / n * i + starter_radius) * circum_radius());
     }
     return res;
   }
@@ -80,9 +73,10 @@ public:
     auto [ymn, ymx] = std::minmax_element(p.begin(), p.end(), [](auto const& lhs, auto const& rhs) { return lhs.y < rhs.y; });
     return Point(xmx->x - xmn->x, ymx->y - ymn->y);
   }
+  
   friend std::ostream& operator << (std::ostream& o, const RegularPolygon& p) {
     auto size = p.size();
-    return o << "{n:" << p.n << ", r:" << p.circum_radius << ", ir:" << p.in_radius() << ", width:" << size.x << ", height:" << size.y << ", center:" << p.center << ", points: " << p.to_points() << "}";
+    return o << "{n:" << p.n << ", r:" << p.circum_radius() << ", ir:" << p.in_radius() << ", width:" << size.x << ", height:" << size.y << ", center:" << p.center << ", points: " << p.to_points() << "}";
   }
 };
 
@@ -103,7 +97,8 @@ int main () {
     polys.push_back(poly);
   }
   
-  std::cout << inp << '\n';
+  std::cout << "sort: " << inp << '\n';
+  std::cout << "each-polygon:\n";
   for (auto& p : polys) {
     std::cout << p << '\n';
   }
